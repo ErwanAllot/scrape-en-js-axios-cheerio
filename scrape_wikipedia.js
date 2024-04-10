7d5ca809347489b8c7f35b4245f376d05d571fe5
ErwanAllot <78737319+ErwanAllot@users.noreply.github.com>
2024-04-10 16:07:32 +0200

    je veux commit

---
+++ scrape_wikipedia.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+// URL de la page Wikipédia à scraper
+const url = 'https://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal';
+
+// Effectuer une requête GET vers l'URL
+axios.get(url)
+    .then(response => {
+        // Charger le contenu HTML de la page
+        const html = response.data;
+
+        // Utiliser Cheerio pour analyser le HTML
+        const $ = cheerio.load(html);
+
+        // Récupérer le contenu du h1
+        const h1Content = $('h1').text();
+
+        // Afficher le contenu du h1 dans la console
+        console.log('Contenu du h1:', h1Content);
+    })
+    .catch(error => {
+        console.error('Une erreur est survenue lors de la requête :', error);
+    });
